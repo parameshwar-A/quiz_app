a30da8931306a56b537cbd2ec1921ca82c7f646b
Parameshwar Arunachalam <parameshwar273@gmail.com>
2024-10-08 07:20:00 +0530

    Added base html render and questions json file

--- base/quiz_app/urls.py
+++ base/quiz_app/urls.py
@@ -3,4 +3,5 @@ from quiz_app import views as quiz_views
 
 urlpatterns = [
         path('', quiz_views.home),
+        path('<slug:name>/', quiz_views.question_set),
         ]
--- base/quiz_app/views.py
+++ base/quiz_app/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import json
+from django.conf import settings
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to the quiz app!")
+    return render(request, 'quiz_app/home.html')
+
+def question_set(request, name):
+    path = str(settings.BASE_DIR)+'/quiz_app/questions.json'
+    print(path)
+    with open(path, 'r') as file:
+        questions = json.load(file)['questions'][:11]
+    return render(request, 'quiz_app/questions.html', {'questions':questions})
+
+    
